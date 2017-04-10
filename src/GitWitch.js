7c0963cae07e960cac2ba1384e9dbd8cbfded425
tim5046 <tim5046@gmail.com>
2017-04-10 09:42:30 -0700

    wip

---
+++ src/GitWitch.js
@@ -0,0 +1,11 @@
+
+class GitWitch {
+  constructor(parser, runner, formatter){
+    this.parser = parser;
+    this.runner = runner;
+    this.formatter = formatter;
+  }
+
+}
+
+module.exports = GitWitch;
