bec15f1f4635e3c9d060b716d14413e2baefcee6
DHail <dhail@ymail.com>
2017-04-10 13:37:54 -0700

    working on GitWitch

---
+++ src/GitWitch.js
@@ -0,0 +1,23 @@
+
+
+class GitWitch {
+  constructor( {parser, runner, formatter} ) {
+    this.parser = parser;
+    this.runner = runner;
+    this.formatter = formatter;
+  }
+
+
+
+  process(input) {
+    this.parser.parse(){
+
+    }
+    this.runner.run() {
+
+    }
+    this.formatter.format() {
+      
+    }
+  }
+}
