e5c087170a4cd52f1e1c4a7b6e2da1a7877f5a79
nicoasp <nicolas.amaya@gmail.com>
2017-04-10 19:18:13 +0200

    implement GitWitch class

--- src/GitWitch.js
+++ src/GitWitch.js
@@ -1,9 +1,25 @@
+const defaultParser = require('./QuestionParser')
+// const runner = require('./CommandRunner')
+// const parser = require('./ResponseFormatter')
 
 class GitWitch {
-  constructor(parser, runner, formatter){
-    this.parser = parser;
+
+  constructor({parser, runner, formatter} = {}){
+    this.parser = parser || defaultParser;
     this.runner = runner;
     this.formatter = formatter;
+  }
+
+  process(input) {
+  	return new Promise((resolve, reject) => {
+			let command = this.parser.parse(input);
+			this.runner.run(command)
+				.then((response) => {
+					let output = this.formatter.format(response)
+					resolve(output);
+				})
+  	})
+
   }
 
 }
