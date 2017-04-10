7c06f33f748966d85f4aac607d03d109632094dd
Jerry Gao <gao@blackwright.com>
2017-04-10 14:47:58 -0400

    phrasing, lana

--- spec/CommandRunnerSpec.js
+++ spec/CommandRunnerSpec.js
@@ -14,6 +14,7 @@ describe("CommandRunner", () => {
 
     spyOn(this.github, "repos").andReturn(Promise.resolve(this.repoList));
     spyOn(this.github, "stars").andReturn(Promise.resolve(this.starList));
+    
     this.commandRunner = new CommandRunner(this.github);
   });
 
