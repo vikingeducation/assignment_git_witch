92e0ac00c23184c3f2474e22a872a5e129105752
Jerry Gao <gao@blackwright.com>
2017-04-10 17:41:55 -0400

    fix ResponseFormatter

--- src/ResponseFormatter.js
+++ src/ResponseFormatter.js
@@ -1,16 +1,16 @@
 class ResponseFormatter {
   format(responseObj) {
-    responseObj.then(response => {
+    return responseObj.then(response => {
       let output = "";
       if (response.query === "details") {
-        output += `${response.username}'s ${response.subject}:'\n`;
+        output += `${response.username}'s ${response.subject}:\n`;
         response.result.forEach(repo => {
           output += `${repo.name} - ${repo.description}\n`;
         });
       } else if (response.query === "count") {
         output += `the user ${response.username} has ${response.result} ${response.subject}`;
       }
-      return new Promise(resolve => { resolve(output) });
+      return output;
     });
   }
 }
