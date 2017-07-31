87cd81faf7b24679c79e3f5be6b5176aca03f138
Ian Halverson <halversoncode@gmail.com>
2017-07-31 12:43:33 -0400

    basic setup

---
+++ src/GitWitch.js
@@ -0,0 +1,16 @@
+const { } = require()
+const { } = require()
+const
+
+class GitWitch {
+  constructor() {
+        process() {
+
+        }
+  }
+  // parser: this.parser,
+  // runner: this.runner,
+  // formatter: this.formatter
+}
+
+module.exports = GitWitch;
