fce6603e572c2b3a03a2b32f753ba821d2468cc9
Eric Glover <etgt28@mail.missouri.edu>
2017-07-31 15:58:16 -0500

    almost finsihed the api call

--- cli.js
+++ cli.js
@@ -1,3 +1,11 @@
-const QuestionParser = require('./lib/questionParser');
-const CommandRunner = require('./lib/commandRunner');
-const Command = require('./lib/commandRunner/command');
+const QuestionParser = require("./lib/questionParser");
+const CommandRunner = require("./lib/commandRunner");
+const Command = require("./lib/commandRunner/command");
+
+let qp = new QuestionParser();
+let cr = new CommandRunner();
+let cmd = qp.parse("what repos does ericglover have?");
+
+cr.run(cmd).then(results => {
+  console.log(results);
+});
--- spec/ResponseFormatterSpec.js
+++ spec/ResponseFormatterSpec.js
@@ -1,28 +1,24 @@
-const ResponseFormatter = require('../lib/responseFormatter');
+const ResponseFormatter = require("../lib/responseFormatter");
 
-describe('Response Formatter', () => {
-	beforeEach(() => {
-		this.responseFormatter = new ResponseFormatter();
-	});
+describe("Response Formatter", () => {
+  beforeEach(() => {
+    this.responseFormatter = new ResponseFormatter();
+  });
 
-	it('receives and formats a response from the command runner', () => {
-		let response = {
-			command: { testCommand: true },
-			results: [
-				{ name: 'foo', desc: 'bar1' },
-				{ name: 'bar', desc: 'foo2' },
-				{ name: 'foobar', desc: 'bar3' },
-				{ name: 'barfoo', desc: 'badr4' },
-				{ name: 'fubar', desc: 'ba5r' },
-				{ name: 'Jake', desc: 'b6ar' }
-			]
-		};
+  it("receives and formats a response from the command runner", () => {
+    let response = {
+      command: { testCommand: true },
+      results: [
+        { name: "foo", desc: "bar1" },
+        { name: "bar", desc: "foo2" },
+        { name: "foobar", desc: "bar3" },
+        { name: "barfoo", desc: "badr4" },
+        { name: "fubar", desc: "ba5r" },
+        { name: "Jake", desc: "b6ar" }
+      ]
+    };
 
-		let formattedResult = this.responseFormatter.format(response);
-		expect(Array.isArray(formattedResult)).toBe(true);
-		expect(formattedResult.some(result => typeof result !== 'string')).toBe(
-			false
-		);
-		expect(formattedResult.length).toEqual(6);
-	});
+    let formattedResult = this.responseFormatter.format(response);
+    expect(typeof formattedResult !== "string").toBe(false);
+  });
 });
