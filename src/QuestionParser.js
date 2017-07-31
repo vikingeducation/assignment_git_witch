189cabead8be4b223aa238a2311af5ea76fcec25
Alex-Willenbrink <awillenbrink@gmail.com>
2017-07-31 12:10:41 -0500

    question parser

--- src/QuestionParser.js
+++ src/QuestionParser.js
@@ -1,11 +1,40 @@
 
 class QuestionParser {
-  constructor {
-    this.parser = { parse() {} };
-    
 
+  constructor() {}
+
+  parse (input) {
+      return {
+        username: this.getUsername(input),
+        query: this.getQuery(input),
+        subject: this.getSubject(input)
+      }
+  }
+
+  getUsername(input) {
+    let inputArray = input.split(" ");
+    return inputArray[inputArray.length - 2];
+  }
+
+  getQuery(input) {
+    let question = input.split(" ")[0].toLowerCase();
+    if(question === "how") {
+      return "count";
+    } else if(question === "what") {
+      return "details";
+    } else {
+      return "error";
+    }
+  }
+
+  getSubject(input) {
+    if(input.split(" ").includes("starred")) {
+      return "starred repos";
+    } else {
+      return "repos";
+    }
+  }
 }
-}
 
 
 
@@ -15,4 +44,8 @@ class QuestionParser {
 
 
 
-module.exports = 
+
+
+
+
+module.exports =
