1ae81dc7e4becbd4d731f57314c789a159b8b618
Ian Halverson <halversoncode@gmail.com>
2017-07-31 16:53:04 -0400

    trying to read API data

--- src/CommandRunner.js
+++ src/CommandRunner.js
@@ -1,24 +1,33 @@
 const request = require("request");
+var stream = require("stream");
 
 class CommandRunner {
   constructor(command) {
     this.command = command;
   }
-  run(this.command) {
-    if (this.command.subject === "starred repos") {
-      this.command.subject = "starred";
-    }
-    Promise.resolve(
-      request.get(
-        `https://api.github.com/users/${this.command.username}/${this.command.subject}`
-      )
-    ).then(data => {
-      if (this.command.query === "count") {
-        this.command.repos = data.length;
-      } else {
-        this.command.repos = data;
-        return command;
+  run() {
+    return new Promise(resolve => {
+      if (this.command.subject === "starred repos") {
+        this.command.subject = "starred";
       }
+      Promise.resolve(
+        request
+          .get(
+            `https://api.github.com/users/${this.command.username}/${this
+              .command.subject}`
+          )
+          .on("data", data => {
+            //currently returns a buffer!
+            console.log(data);
+          })
+      ).then(data => {
+        if (this.command.query === "count") {
+          this.command.repos = data.length;
+        } else {
+          this.command.repos = data;
+        }
+        resolve(this.command);
+      });
     });
   }
 }
