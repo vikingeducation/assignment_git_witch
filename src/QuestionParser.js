3f3cc352619b933ef0e042282814d5dbc5dd15b7
Stephanie Barker <song_mage@yahoo.com>
2017-07-31 13:20:07 -0400

    QuestionParser works!

--- src/QuestionParser.js
+++ src/QuestionParser.js
@@ -1,1 +1,29 @@
-module.exports = {};
+class QuestionParser {
+	parse(input) {
+		input = input.split(" ");
+
+		let obj = {};
+
+		if (input[0] === "what") {
+			obj.query = "details";
+			input = input.slice(1);
+		} else {
+			obj.query = "count";
+			input = input.slice(2);
+		}
+
+		if (input[0] === "starred") {
+			obj.subject = "starred repos";
+			input = input.slice(3);
+		} else {
+			obj.subject = "repos";
+			input = input.slice(2);
+		}
+
+		obj.username = input[0];
+
+		return obj;
+	}
+}
+
+module.exports = QuestionParser;
