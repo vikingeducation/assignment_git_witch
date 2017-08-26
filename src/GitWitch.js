f94a560e07316b7edefcd23f89ab982813da5883
Tyler Ketron <tketron@gmail.com>
2017-08-26 23:14:36 +0200

    implemented functionality to parse questions in QuestionParser module

---
+++ src/GitWitch.js
@@ -0,0 +1,9 @@
+var GitWitch = function(inputs) {
+  this.parser = inputs.parser;
+  this.runner = inputs.runner;
+  this.formatter = inputs.formatter;
+};
+
+GitWitch.prototype.process = function(input) {};
+
+module.exports = GitWitch;
