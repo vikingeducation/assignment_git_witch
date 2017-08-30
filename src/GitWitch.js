667b321a4c8513144a2aad7c639aee49c782037b
Tyler Ketron <tketron@gmail.com>
2017-08-29 21:02:44 -0700

    completed gitwitch module implementation

--- src/GitWitch.js
+++ src/GitWitch.js
@@ -4,6 +4,19 @@ var GitWitch = function(inputs) {
   this.formatter = inputs.formatter;
 };
 
-GitWitch.prototype.process = function(input) {};
+GitWitch.prototype.process = function(input) {
+  return new Promise((resolve, reject) => {
+    let command = this.parser.parse(input);
+    this.runner
+      .run(command)
+      .then(result => {
+        let output = this.formatter.format(result);
+        resolve(output);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
 
 module.exports = GitWitch;
